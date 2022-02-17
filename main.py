5db9645c49f6edf6bc9bd349fc6a4f84f0e2ba3d
Kiran <kiransettyks@gmail.com>
2022-02-17 09:29:04 +0200

    Edited home page to display titles and subtitles from API request Created hyperlinks on homepage to link to each respective blog page

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
+api = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
+api.raise_for_status()
+posts = api.json()
+
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=posts)
+
+
+@app.route('/posts/<page>')
+def post_page(page):
+    post = {}
+    for i in posts:
+        if i['id'] == page:
+            post = i
+    return render_template("post.html", post=post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
