645ecd36875ea027caa14944b5a790737e3432d4
Kiran <kiransettyks@gmail.com>
2022-02-24 21:18:09 +0200

    Added POST Request to Contact Page Added email feature to email entries made in Contact page

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
 from datetime import datetime
 
 app = Flask(__name__)
 
-api = requests.get(url="https://api.npoint.io/6c3754b7be2b015f9e72")
+api = requests.get(url="https://api.npoint.io/ac95d15dc3289a5388fd")
 api.raise_for_status()
 posts = api.json()
 
@@ -13,14 +15,28 @@ posts = api.json()
 def home():
     return render_template("index.html", posts=posts)
 
-@app.route('/contact')
-def contact():
-    return render_template("contact.html", posts=posts)
-
 @app.route('/about')
 def about():
-    return render_template("about.html", posts=posts)
+    return render_template("about.html")
 
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        with smtplib.SMTP("smtp.gmail.com") as email:
+            my_email = "ksetty12@gmail.com"
+            password = os.environ.get("EMAIL_PASS")
+            to_email = "kiransettyks@gmail.com"
+            email.starttls()
+            email.login(user=my_email, password=password)
+            email.sendmail(from_addr=my_email, to_addrs=to_email,
+                           msg=f"Subject:New Entry\n\nName: {data['name']}\nEmail: {data['email']}\nNumber: {data['phone']}\nMessage: {data['message']}")
+
+        return render_template("contact.html", heading="Successfully sent your message")
+
+    else:
+        return render_template("contact.html", heading="Contact Me")
+    # return render_template("form_entry.html", entry=request.form)
 
 @app.route('/posts/<page>')
 def post_page(page):
