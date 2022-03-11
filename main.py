f611a3d2963039fe9fa463c35cb67772c40e2fba
Kiran <kiransettyks@gmail.com>
2022-03-11 14:36:00 +0200

    Added gitignore file to avoid uploading sensitive information

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from flask import Flask, render_template, redirect, url_for, flash, abort
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
-from wtforms.fields.html5 import EmailField
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
-from flask_gravatar import Gravatar
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
